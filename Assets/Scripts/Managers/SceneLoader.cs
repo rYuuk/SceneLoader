17c539049baae2e8201a52cfc94647edb9d0794a
Robin <unbalanced.freaks@gmail.com>
2019-05-24 01:56:40 +0530

    - Refactored scene manager to just encapsulate scene loading functionality

---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneLoader
+{
+    public class SceneLoader
+    {
+        public static AsyncOperation LoadScene(string sceneName, bool isAdditve = false)
+        {
+            if (string.IsNullOrEmpty(sceneName))
+            {
+                Debug.LogWarning("#SceneLoader, Null string passed in LoadScene");
+                return null;
+            }
+
+            Debug.Log("#SceneLoader, Loading scene with name: " + sceneName);
+
+            if (Application.CanStreamedLevelBeLoaded(sceneName))
+                return SceneManager.LoadSceneAsync(sceneName, isAdditve ? LoadSceneMode.Additive : LoadSceneMode.Single);
+            else
+            {
+                Debug.LogWarning("#SceneLoader, No such scene");
+                return null;
+            }
+        }
+
+        public static AsyncOperation UnloadScene(string sceneName)
+        {
+            if (string.IsNullOrEmpty(sceneName))
+            {
+                Debug.LogWarning("#SceneLoader, Null string passed in LoadScene");
+                return null;
+            }
+
+            Debug.Log("#SceneLoader, Loading scene with name: " + sceneName);
+            if (Application.CanStreamedLevelBeLoaded(sceneName))
+                return SceneManager.UnloadSceneAsync(sceneName);
+            else
+            {
+                Debug.LogWarning("#SceneLoader, No such scene");
+                return null;
+            }
+        }
+    }
+}
